9d5b61c485fdb72863ee4aff04c7e8587db6c813
esdi <145417107+esdi-00@users.noreply.github.com>
2024-03-17 19:36:59 -0700

    RCPS-XX: Code cleanup for Student Information Management System (#15)
    RCPS-XX: Code cleanup for Student Information Management System

--- src/main/java/com/student/information/management/data/connection/ConnectionHelper.java
+++ src/main/java/com/student/information/management/data/connection/ConnectionHelper.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
  * This is the Connection for Database.
  */
 public class ConnectionHelper {
-    public static final String URL = "jdbc:oracle:thin:@localhost:1521:rogate";
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:oracleDB";
     public static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
     public static final String username = "system";
     public static final String password = "Changeme0";
