82a5ebcecfb1da08d75c7d92f584544ac8fe8303
katieann7 <150576208+katieann7@users.noreply.github.com>
2024-04-01 00:34:10 +0800

    RCPS-129: Student Information Management System: Fix StudentFacadeImplTest unit test (#36)
    * RCPS-129: Student Information Management System: Fix StudentFacadeImplTest unit test
    
    * RCPS-129: Student Information Management System: Fix StudentFacadeImplTest unit test
    
    ---------
    
    Co-authored-by: Kate Ann I. Amulong <amulongkateann@gmail.com>

--- src/test/java/com/student/information/management/appl/facade/student/impl/StudentFacadeImplTest.java
+++ src/test/java/com/student/information/management/appl/facade/student/impl/StudentFacadeImplTest.java
@@ -2,8 +2,15 @@ package com.student.information.management.appl.facade.student.impl;
 
 import com.student.information.management.appl.facade.student.StudentFacade;
 import com.student.information.management.appl.model.student.Student;
+import com.student.information.management.data.student.dao.StudentDao;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,57 +23,87 @@ import static org.mockito.Mockito.*;
  * This is to test student facade impl.
  */
 class StudentFacadeImplTest {
-//    private StudentFacade studentFacade;
-//    @BeforeEach
-//    public void setUp() {
-//        studentFacade = new StudentFacadeImpl();
-//    }
-//    /**
-//     * This is to test get all students.
-//     */
-//    @Test
-//    public void testGetAllStudents() {
-//        Student student = new Student();
-//        List<String> studentIds = new ArrayList<>();
-//        studentIds.add("CT21-0073");
-//    }
-//    /**
-//     * This is to test get student by id.
-//     */
-//    @Test
-//    public void testGetStudentByID(){
-//        Student student = new Student();
-//        List<String> studentIds = new ArrayList<>();
-//        studentIds.add("CT21-0073");
-//    }
-//    /**
-//     * This is to test add student.
-//     */
-//    @Test
-//    public void testAddStudent() {
-//        Student student = new Student();
-//        List<String> studentIds = new ArrayList<>();
-//        studentIds.add("CT21-0073");
-//    }
-//    /**
-//     * This is to test update student.
-//     */
-//    @Test
-//    public void testUpdateStudent() {
-//        // Arrange
-//        String studentId = "CT21-0073";
-//        Student student = new Student();
-//        student.setStudentId(studentId);
-//
-//        // Create mock object
-//        Student mockStudent = mock(Student.class);
-//
-//        // Act
-//        when(mockStudent.getStudentId()).thenReturn(studentId);
-//        String retrievedStudentId = mockStudent.getStudentId();
-//
-//        // Assert
-//        assertEquals(studentId, retrievedStudentId);
-//        verify(mockStudent, times(1)).getStudentId();
-//    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentFacadeImplTest.class);
+
+    @InjectMocks
+    private StudentFacade studentFacade = new StudentFacadeImpl();
+
+    @Mock
+    private StudentDao studentDao;
+
+    @Mock
+    private List<Student> studentList;
+
+    @Mock
+    private Student student;
+
+    @Mock
+    private Student addStudent;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        student.setStudentId("1");
+        addStudent.setStudentId("2");
+        when(studentDao.getAllStudents()).thenReturn(studentList);
+    }
+
+    @AfterEach
+    public void validate() {
+        validateMockitoUsage();
+    }
+
+    @Test
+    public void testGetAllStudents() {
+        List expectedList = studentFacade.getAllStudents();
+
+        assert(expectedList.equals(studentList));
+        verify(studentDao).getAllStudents();
+    }
+
+    @Test
+    public void testGetStudentById() {
+        when(studentDao.getStudentById("1")).thenReturn(student);
+        Student expectedStudent = studentFacade.getStudentById("1");
+
+        assert(expectedStudent.equals(student));
+
+        verify(studentDao).getStudentById("1");
+    }
+
+    @Test
+    public void testAddStudent() {
+        try {
+            when(studentFacade.getStudentById(addStudent.getStudentId())).thenReturn(null);
+            when(studentDao.addStudent(addStudent)).thenReturn(true);
+
+            boolean result = studentFacade.addStudent(addStudent);
+
+            assert(result == true);
+
+            assert(studentFacade.getStudentById("2") == null);
+            verify(studentDao).addStudent(addStudent);
+        } catch (Exception e) {
+            LOGGER.error("Exception caught: " + e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testUpdateStudent() {
+        try {
+            when(studentDao.getStudentById(student.getStudentId())).thenReturn(student);
+            when(studentDao.updateStudent(student)).thenReturn(true);
+
+            boolean result = studentFacade.updateStudent(student);
+
+            assert(result == true);
+
+            assert(studentFacade.getStudentById("1").equals(student));
+
+            verify(studentDao).updateStudent(student);
+        } catch (Exception e) {
+            LOGGER.error("Exception caught: " + e.getMessage());
+        }
+    }
 }
