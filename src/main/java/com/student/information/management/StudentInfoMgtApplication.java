2aa84a4ba9d8afee1c3c44b954fb5d0a1986576e
harold dinglasan <harolddinglasan90@gmail.com>
2024-04-28 13:09:00 +0800

    RCPS-234 rc-sims:Move the StudentInfoMgtApplication class

--- src/main/java/com/student/information/management/StudentInfoMgtApplication.java
+++ src/main/java/com/student/information/management/StudentInfoMgtApplication.java
@@ -1,14 +1,16 @@
+package com.student.information.management;
+
 import com.student.information.management.appl.facade.student.StudentFacade;
 import com.student.information.management.appl.facade.student.impl.StudentFacadeImpl;
 import com.student.information.management.data.student.dao.StudentDao;
 import com.student.information.management.data.student.dao.impl.StudentDaoImpl;
 /**
- * This class is StudentInfoMgtApplication. This class represents the student information for website.
+ * This class is com.student.information.management.StudentInfoMgtApplication. This class represents the student information for website.
  * */
 public class StudentInfoMgtApplication {
     private StudentFacade studentFacade;
     /**
-     * This creates a new StudentInfoMgtApplication
+     * This creates a new com.student.information.management.StudentInfoMgtApplication
      * @return the studentFacade this helps for managing student data.
      */
     public StudentInfoMgtApplication() {
